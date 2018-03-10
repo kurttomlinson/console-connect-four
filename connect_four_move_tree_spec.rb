f4bfc4cd6e7f31877275802531ca91ae6c0c4537
Kurt Tomlinson <kurttomlinson@gmail.com>
2018-03-09 20:55:24 -0600

    make test more specific

--- connect_four_move_tree_spec.rb
+++ connect_four_move_tree_spec.rb
@@ -10,15 +10,15 @@ describe 'ConnectFourMoveTree#make_move' do
   	tree.make_move(0)
   	tree.make_move(0)
   	tree.make_move(0)
-  	expect{ tree.make_move(0) }.to raise_error
+  	expect{ tree.make_move(0) }.to raise_error 'column overflow'
 	end
   it 'throws an error when given a negative column' do
   	tree = ConnectFourMoveTree.new
-  	expect{ tree.make_move(-1) }.to raise_error
+  	expect{ tree.make_move(-1) }.to raise_error 'columns must be between 0 and 6'
 	end
   it 'throws an error when given a column that is too big' do
   	tree = ConnectFourMoveTree.new
-  	expect{ tree.make_move(ConnectFourConstants::COLUMNS) }.to raise_error
+  	expect{ tree.make_move(ConnectFourConstants::COLUMNS) }.to raise_error 'columns must be between 0 and 6'
 	end
   it 'accepts six moves per column' do
   	(0..ConnectFourConstants::COLUMNS-2).each do |column|
