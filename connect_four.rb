8a0f47b8ba740e0f2c4b7044fbcc9326e3cbafa3
Kurt Tomlinson <kurttomlinson@gmail.com>
2018-03-09 22:09:31 -0600

    display changes

--- connect_four.rb
+++ connect_four.rb
@@ -3,9 +3,11 @@ require './connect_four_move_tree.rb'
 class ConnectFour
 
   def print_state
+    puts ""
     puts @tree.to_s
-    puts "Minimax for player #{@tree.player_to_move}:"
-    unless @tree.winner
+    if !@tree.winner && @tree.last_move
+      puts ""
+      puts "Minimax for player #{@tree.player_to_move}:"
       @tree.last_move.children&.each do |child|
         # 0 = only look at my moves
         # 1 = look at my moves and his moves
@@ -19,14 +21,18 @@ class ConnectFour
 
   def play
     @tree = ConnectFourMoveTree.new
+    print_state
 
     while @tree.winner.nil?
-      next_move = gets.to_i
+      puts ""
+      puts "move?"
+      next_move = gets.strip
+      puts "next_move: #{next_move}"
       @tree.make_move(next_move)
       print_state
     end
 
-    puts "@tree.winner: #{@tree.winner}"
+    puts "Winner: #{@tree.winner}"
   end
 
 end
