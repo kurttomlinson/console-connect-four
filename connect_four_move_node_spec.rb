4d41218fca5338a8d77e2071c89b4e5c644c7ca2
Kurt Tomlinson <kurttomlinson@gmail.com>
2018-03-15 18:27:41 -0500

    clean up a test

--- connect_four_move_node_spec.rb
+++ connect_four_move_node_spec.rb
@@ -179,54 +179,16 @@ describe 'ConnectFourMoveNode#winner' do
   end
   it 'returns a tie when the board is full' do
     tree = ConnectFourMoveTree.new
-    tree.make_move(0) # 1
-    tree.make_move(1)
-    tree.make_move(0) # 1
-    tree.make_move(1)
-    tree.make_move(0) # 1
-    tree.make_move(1)
-
-    tree.make_move(2) # 1
-    tree.make_move(3)
-    tree.make_move(2) # 1
-    tree.make_move(3)
-    tree.make_move(2) # 1
-    tree.make_move(3)
-
-    tree.make_move(4) # 1
-    tree.make_move(5)
-    tree.make_move(4) # 1
-    tree.make_move(5)
-    tree.make_move(4) # 1
-    tree.make_move(5)
-
-    tree.make_move(6) # 1
-    tree.make_move(0)
-    tree.make_move(6) # 1
-    tree.make_move(0)
-    tree.make_move(6) # 1
-    tree.make_move(0)
-
-    tree.make_move(1) # 1
-    tree.make_move(2)
-    tree.make_move(1) # 1
-    tree.make_move(2)
-    tree.make_move(1) # 1
-    tree.make_move(2)
-
-    tree.make_move(3) # 1
-    tree.make_move(4)
-    tree.make_move(3) # 1
-    tree.make_move(4)
-    tree.make_move(3) # 1
-    tree.make_move(4)
-
-    tree.make_move(5) # 1
-    tree.make_move(6)
-    tree.make_move(5) # 1
-    tree.make_move(6)
-    tree.make_move(5) # 1
-    tree.make_move(6)
+    moves = [0,1,0,1,0,1,
+             2,3,2,3,2,3,
+             4,5,4,5,4,5,
+             6,0,6,0,6,0,
+             1,2,1,2,1,2,
+             3,4,3,4,3,4,
+             5,6,5,6,5,6]
+    moves.each do |move|
+      tree.make_move(move)
+    end
 
     expect(tree.last_move.winner).to eq ConnectFourConstants::TIE
   end
